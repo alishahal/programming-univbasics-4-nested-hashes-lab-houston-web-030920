cae43a2e4c8c9b83d5bd4a583f0fa972e867c011
Karim Macknojia <alishahal@msn.com>
2020-02-26 17:48:33 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -3,17 +3,8 @@ def hopper
   
 	programmer_hash = 
  		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
+        {
+          
         }
      }
 
